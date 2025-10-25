fd0ea9142ebd3bd2f7f707dc20b0d0643dd9e831
wahidul islam sami <wahidulislamsami5@gmail.com>
2025-10-25 18:19:33 +0600

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://backend-rmg0.onrender.com,
+  baseURL: "https://backend-rmg0.onrender.com/api/v1",
   withCredentials: true,
 });
 
