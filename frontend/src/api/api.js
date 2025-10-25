a0fe58733729f8b782486ae2e98c10f5074363c3
wahidul islam sami <wahidulislamsami5@gmail.com>
2025-10-25 18:14:44 +0600

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://backend-b84i.vercel.app",
+  baseURL: "https://backend-rmg0.onrender.com,
   withCredentials: true,
 });
 
