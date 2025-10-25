f486f7ca5838071ab5d08df4ef11ef8ef6b7487a
wahidul islam sami <wahidulislamsami5@gmail.com>
2025-10-25 15:30:26 +0600

    new

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://backendv1-a60l.onrender.com/api/v1", // backend URL
-  withCredentials: true, // send cookies with requests
+  baseURL: "https://backendv1-a60l.onrender.com/api/v1",
+  withCredentials: true,
 });
 
 export default api;
